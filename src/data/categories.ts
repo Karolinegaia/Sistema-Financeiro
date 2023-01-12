d1ea5662ff5636533603ea21c8f0ec97376c484f
Karoline Gaia <drakarolinegaia@gmail.com>
2023-01-12 14:12:28 -0300

    Creating the types

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,5 +1,7 @@
+import { category } from "../types/category"
+
 export const categories = {
     food: {title: 'Alimentação', color: 'blue', expense: true},
     rent: {title: 'Aluguel', color: 'brown', expense: true},
-
+    salary: {title: 'Salário', color: 'green', expense: false},
 }
