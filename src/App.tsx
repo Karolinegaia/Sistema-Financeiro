804b5920d2917b45d3438b6b6b0dbbde9f147dd8
Karoline Gaia <drakarolinegaia@gmail.com>
2023-03-10 10:10:10 -0300

    adding date filter

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Item } from "./types/Item"
 import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { items } from "./data/items";
-import { getCurrentMonth} from "./helpers/dataFilter"
+import { getCurrentMonth, filteredListByMonth} from "./helpers/dataFilter"
 
 const App = () => {
   const [list, setList] = useState(items);
@@ -12,7 +12,7 @@ const App = () => {
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
 
   useEffect(()=> {
-
+    setFilteredList(filteredListByMonth(list, currentMonth));
   },[list, currentMonth]);
   return (
     <C.Container>
--- src/helpers/dataFilter.ts
+++ src/helpers/dataFilter.ts
@@ -7,5 +7,16 @@ export const getCurrentMonth = () => {
 export const filteredListByMonth = (list:Item[],date:string): Item[] => {
     let newList:Item[] = [];
 
+    let [year, month] = date.split('-');
+
+    for(let i in list){
+        if(
+            list[i].date.getFullYear() === parseInt(year) &&
+            (list[i].date.getMonth() + 1) === parseInt(month)
+        ){
+            newList.push(list[i]);
+        }
+    }
+
     return newList;
 }
